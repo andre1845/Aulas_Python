5add549534583d1e573a8c9c56124d990cd127ff
Andre Andrino <andrino.senac@gmail.com>
2024-08-09 17:10:14 -0300

    Update media_flex_app.py

--- 1.Trabalhos_Python/14.media/media_flex_app.py
+++ 1.Trabalhos_Python/14.media/media_flex_app.py
@@ -1,14 +1,25 @@
 import os
 lista_notas = []
 i = 1
+os.system('cls')
 print ('Digite as notas abaixo. ')
 while True:
-    os.system('cls')
+    
     nota_input = input(f'Digite a {i}ª nota (Deixe em branco para encerrar)')
+    nota_input = str(nota_input).replace(",", ".")
+    try:
+        nota_input = float(nota_input)
+        if nota_input < 0 or nota_input >10:
+            print('Nota invalida')
+            continue
+    except:
+        if nota_input != "" :
+            print('Nota invalida')
+            continue
     
     i = i+1
     if nota_input != "":
-        nota = float(nota_input)
+        nota = nota_input
         lista_notas.append(nota)
         continue
     else:
