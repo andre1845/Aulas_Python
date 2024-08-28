76f00136066f3a89a5b5900cd71a263d85ed85fa
Andre Andrino <andrino.senac@gmail.com>
2024-08-28 17:46:09 -0300

    Delete pandateste.py

--- JUPYTER/pandateste.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/brunamulinari/BasicPythonProjects/main/Base_ficticia/baseficticia.csv', sep = ';')
-
-type(df) 
-
-df.head(n=6)
