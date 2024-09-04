0a6966d8f405b19084f0a25ffd40e0b7a4bca3b8
Andre Andrino <andrino.senac@gmail.com>
2024-09-04 16:50:14 -0300

    lista 34

--- 1.Trabalhos_Python/34.Atv_Revisao/08_media.py
+++ 1.Trabalhos_Python/34.Atv_Revisao/08_media.py
@@ -6,19 +6,23 @@ i = 1
 print ('Digite os numeros. ')
 while True:
     os.system('cls')
-    numero_input = input(f'Digite o {i}º numero (Deixe em branco para encerrar)')
+    numero_input = input(f'Digite o {i}º numero (Deixe em branco para encerrar)').replace(',','.')
     
     i = i+1
     if numero_input != "":
-        numero = float(numero_input)
-        lista_numeros.append(numero)
-        continue
+        try:
+            numero = float(numero_input)
+            lista_numeros.append(numero)
+            continue
+        except:
+            print('Entrada inválida.')
+            i = i -1
+            continue
     else:
-        
         qtde = len(lista_numeros)
         soma = sum(lista_numeros)
         media = soma/qtde
-        print(f'\nA média das {qtde} numeros é {media:,.2f}')
+        print(f'\nA média dos {qtde} numeros é {media:,.3f}')
         
     continuar = input('Deseja calcular outra média? (s/n) ')
     if continuar != "n":
