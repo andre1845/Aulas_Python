3051d3b2d7dcdb289f24887f7f542c6f3d2d3fa6
Andre Andrino <andrino.senac@gmail.com>
2024-09-10 17:49:18 -0300

    testes

---
+++ automacao/autogui.py
@@ -0,0 +1,6 @@
+import pyautogui as pag
+
+pag.PAUSE = 0.7
+pag.press('win')
+pag.press('edge')
+pag.press('enter')
---
+++ django/calculadora/equacao/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EquacaoConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'equacao'
