276daa4307453d08f0bfe45ffa4f96ac8e2464e7
Andre Andrino <andrino.senac@gmail.com>
2024-09-25 17:47:27 -0300

    FLET

--- Flet/hello_app.py
+++ Flet/hello_app.py
@@ -4,8 +4,14 @@ def main(page: ft.Page):
     page.title = "Ola mundo"
     page.scroll = "adaptive"
     
+    nome = ft.TextField(label="Nome")
+    
     page.add(
-        ft.Text("Ola mundo")
+        ft.Text("Ola mundo", size=50, color="red"),
+        nome,
+        ft.Text("TESTE", size=40, color="yellow"),
+        ft.TextField(label="teste", text_size=35),
+        ft.TextButton("Clique Aqui")
     )
     page.update()
     
