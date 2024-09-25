18323042de32c91bc190ebf285d4666b721678f9
Andre Andrino <andrino.senac@gmail.com>
2024-09-25 15:32:39 -0300

    json alterado

--- JSON/app.py
+++ JSON/app.py
@@ -11,7 +11,9 @@ if __name__=="__main__":
     
         print("1 - Criar novo arquivo JSON")
         print("2 - Ler arquivo JSON")
-        print("3 - Gravar arquivo JSON")
+        print("3 - Gravar no arquivo JSON")
+        print("4 - Alterar no arquivo JSON")
+        print("5 - Excluir no arquivo")
         print("0 - Sair")
         
         opcao = input("Digite a opcao: ")
@@ -53,25 +55,80 @@ if __name__=="__main__":
                     
                     print(f"Gravando no arquivo: {ler_arquivo}.json\n ")
                      # Define o código do novo usuário como o próximo número na lista
+                    '''
                     usuario['Codigo'] = len(usuarios)
                     
                     # Coleta as informações do novo usuário
                     for campo in campos:
                         usuario[campo.capitalize()] = input(f"Informe {campo.capitalize()}: ")
                     
-                   
-                    
                     # Adiciona o novo usuário à lista
                     usuarios.append(usuario)
                     
                     # Salva os dados atualizados no arquivo
+                    print(m.salvar_dados(usuarios, ler_arquivo))
+                    '''
+                    print(f"Arquivo aberto: {ler_arquivo}.json.\n")
+                    p.codigo = len(usuarios)
+                    p.nome = input("Nome: ")
+                    p.cpf = input("cpf: ")
+                    p.email = input("Email: ")
+                    p.profissao = input("Profissao: ")
+                    usuario = dict(codigo=p.codigo, nome=p.nome, cpf=p.cpf, email=p.email, profissao=p.profissao)
+                    usuarios.append(usuario)
                     print(m.salvar_dados(usuarios, ler_arquivo))
                 
                 except Exception as e:
                     print(f"Não foi possível gravar os dados. Erro: {e}")
                 finally:
                     continue
-                
+            case '4':
+                try:
+                    if not ler_arquivo:
+                        print("Nenhum arquivo foi carregado ou criado. Abra ou crie um arquivo primeiro.")
+                    # Garante que o arquivo foi lido
+                    usuarios = m.abrir_arquivo(ler_arquivo)
+                    
+                    print(f"Gravando no arquivo: {ler_arquivo}.json\n ")
+                     # Define o código do novo usuário como o próximo número na lista
+                    codigo = int(input("Informe o codigo do usuario que deeja alterar: "))
+                    for campo in usuarios[codigo]:
+                        print(f"{campo}: {usuarios[codigo].get(campo)}")
+                        novo_dado = input(f"Novo valor de {campo} ou ENTER. ")
+                        if novo_dado:
+                            usuarios[codigo][campo] = novo_dado
+                        else:
+                            ...
+                    
+                    print(m.salvar_dados(usuarios, ler_arquivo))
+                             
+                except:
+                     print(f"Não foi possível alterar os dados. Erro: {e}")
+                finally:
+                    continue
+            case '5':
+                try:
+                    if not ler_arquivo:
+                        print("Nenhum arquivo foi carregado ou criado. Abra ou crie um arquivo primeiro.")
+                    # Garante que o arquivo foi lido
+                    usuarios = m.abrir_arquivo(ler_arquivo)
+                    
+                    print(f"Gravando no arquivo: {ler_arquivo}.json\n ")
+                     # Define o código do novo usuário como o próximo número na lista
+                    codigo = int(input("Informe o codigo do usuario que deseja EXCLUIR: "))
+                    nome_deletado = usuarios[codigo]['nome']
+                    confirmacao = input(f"Apagando {nome_deletado} (s/n) ?").lower()
+                    if confirmacao == 's':
+                        del(usuarios[codigo])
+                        print(m.salvar_dados(usuarios, ler_arquivo))
+                    else:
+                        print("Exclusão CANCELADA !")
+                    
+                except:
+                     print(f"Não foi possível excluir os dados. Erro: {e}")
+                finally:
+                    continue
+                            
             case _:
                 print("Opção invalida")
                 continue
